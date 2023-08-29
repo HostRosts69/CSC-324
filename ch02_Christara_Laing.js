a7cb0517be3290cae41c8a3358e98e84efd3b1ed
Christara Laing <Chrissy123376@gmail.com>
2023-08-29 16:19:25 -0400

    check with promt

--- ch02_Christara_Laing.js
+++ ch02_Christara_Laing.js
@@ -2,49 +2,49 @@
 //// Looping a triangle
 for (let number = 1; number <= 7; number = number+1) {
     console.log("#".repeat(number));
-  }
+  };
 
 //// FizzBuzz
 ///// Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead
 for (let number = 0; number <= 100; number = number+1) {
     if (number % 3 == 0 && number % 5 == 0){
-      console.log("FizzBuzz")
+      console.log("FizzBuzz");
     }else if (number % 3 == 0){
-        console.log("Fizz")
+        console.log("Fizz");
     } else if (number % 5 == 0){
-        console.log("Buzz")
+        console.log("Buzz");
     } else{
-        console.log(number)
+        console.log(number);
     }
   }
 
 //// Chessboard
-let heck = ""
-let size = prompt("Please enter number!", "8")
+let heck = "" ;
+let size = prompt("Please enter number!", "8");
 for (let sizeL = size; sizeL >= 1; sizeL = sizeL-1) {
 
     if (sizeL % 2 == 0){
-        let length = size
+        let length = size;
         while (length >= 1) {
             if (length % 2 != 0){
-                heck = heck + "#"
+                heck = heck + "#";
             } else {
-                heck = heck + " "
+                heck = heck + " ";
             }
-            length = length-1
+            length = length-1;
         }
         
         }else{
-            let length = size
+            let length = size;
             while (length >= 1) {
                 if (length % 2 == 0){
-                    heck = heck + "#"
+                    heck = heck + "#";
                 } else {
-                    heck = heck + " "
+                    heck = heck + " ";
                 }
-                length = length-1
+                length = length-1;
         }
     }
-    heck = heck + "\n"
+    heck = heck + "\n";
 }
-console.log (heck)
+console.log (heck);
