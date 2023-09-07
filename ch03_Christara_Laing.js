7faaebcd843f8d2d870720ffed9c9470c1a64cdd
Christara Laing <Chrissy123376@gmail.com>
2023-09-07 12:27:10 -0400

    init Recursion  commit

--- ch03_Christara_Laing.js
+++ ch03_Christara_Laing.js
@@ -9,7 +9,20 @@ function min(a = 0, b= 1){
   		return(b);
   }
 }
+console.log(min(0, 10));
+
+console.log(min(0, -10));
 
 //// Recursion:
+///// Does not work with negative numbers
+function isEven(n) {
+    if (n == 0){
+        return true;
+    } else if (n <=1){
+        return false;
+    } else{
+        return isEven(n - 2);
+    }
+}
 
 //// Bean Counting:
