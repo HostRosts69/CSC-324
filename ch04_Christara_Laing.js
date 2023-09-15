f7c841782dd66c342ed3ab996312578295542954
Christara Laing <Chrissy123376@gmail.com>
2023-09-14 21:14:19 -0400

    init commit range

--- ch04_Christara_Laing.js
+++ ch04_Christara_Laing.js
@@ -1,7 +1,13 @@
 //Chapter 4
 /// Exercises
 //// Sum and Range
-
+function range(start, end){
+    let holdRange = [];
+        for (let number = 1; number <= end; number = number + 1){
+            holdRange.push(number)
+        };
+    return holdRange;
+  }
 
 
 //// Reversing an Array
