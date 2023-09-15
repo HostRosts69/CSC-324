0191edd8d802348f58d659aa70b89375009b78b4
Christara Laing <Chrissy123376@gmail.com>
2023-09-14 21:52:05 -0400

    init commit rangeStep

--- ch04_Christara_Laing.js
+++ ch04_Christara_Laing.js
@@ -3,11 +3,28 @@
 //// Sum and Range
 function range(start, end){
     let holdRange = [];
-        for (let number = 1; number <= end; number = number + 1){
-            holdRange.push(number)
-        };
+    for (let number = start; number <= end; number = number + 1){
+        holdRange.push(number)
+    };
     return holdRange;
   }
+
+console.log(range(1, 10));
+
+function rangeStep(start, end, step){
+    let holdRange = [];
+    if ((end - start) % step != 0){
+        console.log("Sorry this action cannot be completed! \n" + (end - start) + " is not divisable by " + step)
+    }
+
+    for (let number = start; number >= end; number = number + step){
+        holdRange.push(number)
+    };
+    return holdRange;
+  }
+
+console.log(rangeStep(1, 10, 2));
+
 
 
 //// Reversing an Array
