60738f9f82ae56e1e454f3e1a8d965f7dd14b426
Christara Laing <Chrissy123376@gmail.com>
2023-09-21 21:08:03 -0400

    init commit loop and every

--- ch05_Christara_Laing.js
+++ ch05_Christara_Laing.js
@@ -2,6 +2,7 @@
 
 /// Flattening
 
+//note: reduce auto recurses
 let arrays = [[1, 2, 3], [4, 5], [6], [8, 9, 10], [11]];
 function flatten(array = [[1, 2, 3], [4, 5], [6]]){
     return arrays.reduce((a,b) => a.concat(b));
@@ -39,7 +40,6 @@ loop(3, n => n > 0, n => n - 1, console.log);
 function every(array, test) {
     for (let elem of array){
         if (test(elem) != true){
-            console.log(`The element in "${array}" that failed the test is ${ele}. \n The function used to equate this array is "${test}"`)
             return false;
         }
     } 
@@ -51,6 +51,27 @@ console.log(every([1, 3, 5], n => n < 10));
 console.log(every([2, 4, 16], n => n < 10));
 // → false
 console.log(every([], n => n < 10));
+// → true
+
+
+function every_log(array, test) {
+    for (let elem of array){
+        if (test(elem) != true){
+            console.log(`The element in "${array}" that failed the test is ${elem}. \n The function used to equate this array is "${test}"`)
+            return false;
+        }
+    } 
+    return true;
+}
+
+
+
+
+console.log(every_log([1, 3, 5], n => n < 10));
+// → true
+console.log(every_log([2, 4, 16], n => n < 10));
+// → false
+console.log(every_log([], n => n < 10));
 // → true
 
 
