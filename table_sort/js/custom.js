dd90b46863fa9bb7af539beeca05970377a01870
trashbird <chrissy123376@gmail.com>
2023-10-22 16:24:40 -0400

    without new array

--- table_sort/js/custom.js
+++ table_sort/js/custom.js
@@ -47,67 +47,73 @@ const buttons = [
    name: "Sort Name",
    id: 'byName-button',
    function: function() {
-              var table, rows, switching, row1, row2; // have to declare I up here so it can be used when loop breaks
-              table = document.getElementById("bhangra");
-              switching = true;
-
-              // restarts if switching == true
-              while (switching) {
-                switching = false;
-                rows = table.rows;
-                //loops through all the rows
-                for (let i = 1; i < (rows.length - 2); i++) {
-
-                  row1 = rows[i].getElementsByTagName("TD")[0]; //row[i]: first row; 0: first cell: ArtistName
-                  row2 = rows[i + 1].getElementsByTagName("TD")[0]; //row[i+1]: second row; 0: first cell: ArtistName
-                  
-                  if (row1.innerHTML.toLowerCase() > row2.innerHTML.toLowerCase()) {
-                      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
-                      switching = true; // double checks if they need to be sorted more
-                    }
-                }
-              };}
+      var table, rows, switchTR, row1, row2; // have to declare I up here so it can be used when loop breaks
+      table = document.getElementById("bhangra");
+      switchTR = true;
+    
+      // restarts if switching == true
+      while (switchTR) {
+        switchTR= false;
+        rows = table.rows;
+        //loops through all the rows
+        for (let i = 1; i < (rows.length - 2); i++) {
+    
+          row1 = rows[i].getElementsByTagName("TD")[0]; //row[i]: first row; 0: first cell: ArtistName
+          console.log(rows);
+          console.log(rows[i]);
+          console.log(row1);
+          row2 = rows[i + 1].getElementsByTagName("TD")[0]; //row[i+1]: second row; 0: first cell: ArtistName
+          
+          if (row1.innerHTML.toLowerCase() > row2.innerHTML.toLowerCase()) {// .toLowerCase: makes string lowercase
+              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
+              switching = true; // double checks if they need to be sorted more
+            }
+        }
+      };}
  },
  {
    name: "Sort Birth Year",
    id: 'byYear-button',
    function: function() {
-              var table, rows, switching, row1, row2; // have to declare I up here so it can be used when loop breaks
-              table = document.getElementById("bhangra");
-              switching = true;
-              
-              // restarts if switching == true
-              while (switching) {
-                switching = false;
-                rows = table.rows;
-                //loops through all the rows
-                for (let i = 1; i < (rows.length - 2); i++) {
+        var table, rows, switchTR, row1, row2; // have to declare I up here so it can be used when loop breaks
+        table = document.getElementById("bhangra");
+        switchTR= true;
+        
+        // restarts if switching == true
+        while (switchTR) {
+          switchTR = false;
+          rows = table.rows;
+          //loops through all the rows
+          for (let i = 1; i < (rows.length - 2); i++) {
+      
+            // gets elements based on tag <td>
+            row1 = rows[i].getElementsByTagName("TD")[1]; //row[i]: first row; 1: second cell; elements within cell
+            console.log(rows);
+            console.log(rows[i]);
+            console.log(row1);
+            row2 = rows[i + 1].getElementsByTagName("TD")[1]; //row[i+1]: second row; 2: second cell; elements within cell
             
-                  // gets elements based on tag <td>
-                  row1 = rows[i].getElementsByTagName("TD")[1]; //row[i]: first row; 0: first cell; elements within cell
-                  row2 = rows[i + 1].getElementsByTagName("TD")[1]; //row[i+1]: second row; 0: first cell; elements within cell
-                  
-                  if (row1.innerHTML > row2.innerHTML) {
-                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
-                    switching = true; // double checks if they need to be sorted more
-                }
-              }
-            };}
+            if (row1.innerHTML > row2.innerHTML) {
+              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
+              switchTR = true; // double checks if they need to be sorted more
+          }
+        }
+      };}
   },
   {
   name: "Random Order",
   id: 'random-button',
   function: function() { 
-             var table, rows;// have to declare I up here so it can be used when loop breaks
-             table = document.getElementById("bhangra");
-             rows = table.rows;
-             for (let i = 1; i < rows.length - 1; i ++){
-               var random = Math.floor(Math.random() * (rows.length - 2));//random number between 0 and 4; -2 so buttons in the table not moved
-               if (random == 0){
-                 random += 1; // adds 1 to 0 so that nothing is added before tile
-               }
-               rows[random].parentNode.insertBefore(rows[i], rows[random]);//first value:row to insert; second value: refreance node; insertBefore.inserts rows before other rows
-             };}
+      var table, rows;// have to declare I up here so it can be used when loop breaks
+      table = document.getElementById("bhangra");
+      rows = table.rows;
+      for (let i = 1; i < rows.length - 1; i ++){
+        var random = Math.floor(Math.random() * (rows.length - 2));//random number between 0 and 4; -2 so buttons in the table not moved
+        if (random == 0){
+          random += 1; // adds 1 to 0 so that nothing is added before tile
+        }
+        rows[random].parentNode.insertBefore(rows[i], rows[random]);//first value:row to insert; second value: refreance node; insertBefore.inserts rows before other rows
+      };}
  }
 ];
 
@@ -131,16 +137,16 @@ for (elm of buttons){
 
 // how functions looks when not in the array
 function sortTable_byName() {
-  var table, rows, switching, row1, row2; // have to declare I up here so it can be used when loop breaks
+  var table, rows, switchTR, row1, row2; // have to declare I up here so it can be used when loop breaks
   table = document.getElementById("bhangra");
   switching = true;
 
   // restarts if switching == true
-  while (switching) {
-    switching = false;
+  while (switchTR) {
+    switchTR= false;
     rows = table.rows;
     //loops through all the rows
-    for (let i = 1; i < (rows.length - 1); i++) {
+    for (let i = 1; i < (rows.length - 2); i++) {
 
       row1 = rows[i].getElementsByTagName("TD")[0]; //row[i]: first row; 0: first cell: ArtistName
       row2 = rows[i + 1].getElementsByTagName("TD")[0]; //row[i+1]: second row; 0: first cell: ArtistName
@@ -166,13 +172,13 @@ function sortTable_Random (){
 }
 
 function sortTable_byYear(){
-  var table, rows, switching, row1, row2; // have to declare I up here so it can be used when loop breaks
+  var table, rows, switchTR, row1, row2; // have to declare I up here so it can be used when loop breaks
   table = document.getElementById("bhangra");
-  switching = true;
+  switchTR = true;
   
   // restarts if switching == true
-  while (switching) {
-    switching = false;
+  while (switchTR) {
+    switchTR = false;
     rows = table.rows;
     //loops through all the rows
     for (let i = 1; i < (rows.length - 2); i++) {
@@ -183,8 +189,27 @@ function sortTable_byYear(){
       
       if (row1.innerHTML > row2.innerHTML) {
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
-        switching = true; // double checks if they need to be sorted more
+        switchTR = true; // double checks if they need to be sorted more
     }
   }
 }
 }
+
+
+ 
+//  Sources used to help me:
+// This code used to help me understand how the subsetting was working
+// Similar to data science when table data was scraped from website
+// console.log(rows);
+// console.log(rows[i]);
+// console.log(row1);
+//   - https://www.w3schools.com/jsref/coll_table_rows.asp (searched: find out how many rows in table js)
+//   - https://www.tutorialspoint.com/how-to-access-tr-element-from-table-using-javascript#:~:text=To%20access%20a%20element,tr%3E%20elements%20within%20the%20table (searched: get elements of rows js)
+//   - https://developer.mozilla.org/en-US/docs/Web/API/Element/getElementsByTagName (searched: getElementsByTagName js)
+
+//   - https://stackoverflow.com/questions/28862402/how-to-move-reorder-an-html-table-row (searched: how to reorder table js; clicked: How to move/reorder an html table row)
+//   - https://stackoverflow.com/questions/4515237/add-table-row-before-or-after-a-table-row-of-known-id (searched: add row before js)
+//   - https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore (searched: insertBefore js; to get a better understanding)
+
+//   - https://stackoverflow.com/questions/15315315/how-do-i-add-a-button-to-a-td-using-js (searched: how to add button to table js)
+//   - https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild (searched: appendChild.Js)
