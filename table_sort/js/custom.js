e0e4cb2f03427fb227b5299c411b56d29c9e7731
trashbird <chrissy123376@gmail.com>
2023-10-22 14:55:21 -0400

    without new array

--- table_sort/js/custom.js
+++ table_sort/js/custom.js
@@ -1,3 +1,85 @@
+//sets up buttons and their functions and ids
+const buttons = [
+  {
+   name: "Random Order",
+   id: 'random-button',
+   function: function() { 
+              var table, rows;// have to declare I up here so it can be used when loop breaks
+              table = document.getElementById("bhangra");
+              rows = table.rows;
+              for (let i = 1; i < rows.length; i ++){
+                var random = Math.floor(Math.random() * (rows.length - 1));//random number between 0 and 4
+                if (random == 0){
+                  random += 1; // adds 1 to 0 so that nothing is added before tile
+                }
+                rows[random].parentNode.insertBefore(rows[i], rows[random]);//first value:row to insert; second value: refreance node; insertBefore.inserts rows before other rows
+              };}
+  },
+  {
+   name: "Sort By Name",
+   id: 'byName-button',
+   function: function() {
+              var table, rows, switching, row1, i, row2; // have to declare I up here so it can be used when loop breaks
+              table = document.getElementById("bhangra");
+              switching = true;
+
+              // restarts if switching == true
+              while (switching) {
+                switching = false;
+                rows = table.rows;
+                //loops through all the rows
+                for (i = 1; i < (rows.length - 1); i++) {
+
+                  row1 = rows[i].getElementsByTagName("TD")[0]; //row[i]: first row; 0: first cell: ArtistName
+                  row2 = rows[i + 1].getElementsByTagName("TD")[0]; //row[i+1]: second row; 0: first cell: ArtistName
+                  
+                  if (row1.innerHTML.toLowerCase() > row2.innerHTML.toLowerCase()) {
+                      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
+                      switching = true; // double checks if they need to be sorted more
+                    }
+                }
+              };}
+ },
+ {
+   name: "Sort By Birth Year",
+   id: 'byYear-button',
+   function: function() {
+    var table, rows, switching, row1, row2, i; // have to declare I up here so it can be used when loop breaks
+    table = document.getElementById("bhangra");
+    switching = true;
+    
+    // restarts if switching == true
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      //loops through all the rows
+      for (i = 1; i < (rows.length - 1); i++) {
+  
+        // gets elements based on tag <td>
+        row1 = rows[i].getElementsByTagName("TD")[1]; //row[i]: first row; 0: first cell; elements within cell
+        row2 = rows[i + 1].getElementsByTagName("TD")[1]; //row[i+1]: second row; 0: first cell; elements within cell
+        
+        if (row1.innerHTML < row2.innerHTML) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
+          switching = true; // double checks if they need to be sorted more
+      }
+    }
+  };}
+}
+];
+
+for (const element of buttons){
+  const newButton = document.createElement('button');
+  newButton.textContent = element.name;
+  newButton.id = element.id;
+  document.body.appendChild(newButton);
+}
+
+for (element of buttons){
+  const randomButton = document.querySelector("#"+element.id);
+  randomButton.addEventListener("click", element.function);
+}
+
 const artists = [
   {
     name: "Ms Scandalous",
@@ -26,8 +108,6 @@ const artists = [
  }
 ];
 
-// sort the artists by name
-
 let contents = "<tr> <th>Name</th> <th>Date of Birth</th> <th>Link</th> </tr>";// needs data so insert row can work
 
 const table = document.querySelector("#bhangra");
@@ -42,63 +122,39 @@ table.innerHTML = contents;
    cell3.innerHTML = `<a href=${element.link}>${element.link}</a>`;
 }
 
-// first a function to sort by name:
-function byName(a, b) {
-  if (a.name < b.name) return -1;
-  if (a.name > b.name) return 1;
-  return 0;
- }
 
- // now, sort:
- console.log(artists.sort(byName));
+// how functions looks when not in the array
+function sortTable_byName() {
+  var table, rows, switching, row1, i, row2; // have to declare I up here so it can be used when loop breaks
+  table = document.getElementById("bhangra");
+  switching = true;
 
-// pick a random artist
-function pickArtist() {
-  const randomIndex = Math.floor(Math.random() * artists.length);
-  return artists[randomIndex].name;
-}
+  // restarts if switching == true
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    //loops through all the rows
+    for (i = 1; i < (rows.length - 1); i++) {
 
-let contents = "<tr> <th>Name</th> <th>Date of Birth</th> <th>Link</th> </tr>";// needs data so insert row can work
-
-const table = document.querySelector("#bhangra");
-table.innerHTML = contents;
- for(const element of artists){
-   var row = table.insertRow(-1);// adds to bottom
-   var cell1 = row.insertCell(0);// adds 1st row cell
-   var cell2 = row.insertCell(1);// adds 2nd row cell
-   var cell3 = row.insertCell(2);// adds 3rd row cell
-   cell1.innerHTML = element.name;
-   cell2.innerHTML = element.birthYear;
-   cell3.innerHTML = `<a href=${element.link}>${element.link}</a>`;
-}
-
-// Shuffle the artists randomly:
-
-// first, a shuffling method adapted from
- // https://www.kirupa.com/html5/shuffling_array_js.htm
-
- Array.prototype.shuffle = function() {
-  let input = this;
-  for (let i = input.length-1; i >= 0; i--) {
-    let randomIndex = Math.floor(Math.random()*(i+1));
-    let itemAtIndex = input[randomIndex];
-    input[randomIndex] = input[i];
-    input[i] = itemAtIndex;
+      row1 = rows[i].getElementsByTagName("TD")[0]; //row[i]: first row; 0: first cell: ArtistName
+      row2 = rows[i + 1].getElementsByTagName("TD")[0]; //row[i+1]: second row; 0: first cell: ArtistName
+      
+      if (row1.innerHTML.toLowerCase() > row2.innerHTML.toLowerCase()) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//inserts row needing to be switched, before the second row
+          switching = true; // double checks if they need to be sorted more
+        }
+    }
   }
- return input;
-};
+}
 
-// Suppose that this js file gets loaded by
-// the browser when it parses some html file,
-// and suppose that the html file has a button
-// as in the table-sort assignment.
-// Then you could use the above ideas to add
-// an event-listener to the button
-// to show a randomly-selected artist:
-const randomButton = document.querySelector("#random-button");
-randomButton.addEventListener("click", showRandomArtist);
 
-function showRandomArtist() {
-  const artist = document.querySelector("#bhangra");
-  artist.innerHTML = artists.shuffle();
+// what random looks like out of the array
+function sortTable_Random (){
+  var table, rows;// have to declare I up here so it can be used when loop breaks
+  table = document.getElementById("bhangra");
+  rows = table.rows;
+   for (let i = 1; i < (rows.length - 1); i ++){
+    var random = Math.floor(Math.random() * (rows.length - 1));
+    rows[i].parentNode.insertBefore(rows[random], rows[i]);//second value is refreance node
+   }
 }
